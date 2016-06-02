ae88e6d877435a0a1cd1e38e6c68fe8f56fd5705
Louis Goddard <louisgoddard@gmail.com>
2016-06-02 16:08:24 +0100

    height tinkering:

--- constellations/graph.js
+++ constellations/graph.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     var bounds = $("#graph").width();
-    var width = bounds, height = Math.max(600, $(window).height());
+    var width = bounds, height = $(window).height()-10;
     var force = d3.layout.force()
         .charge(-bounds/6).linkDistance(bounds/25).size([width, height]);
 	
 	var svg = d3.select("#graph").append("svg")
-    	.attr("width", bounds).attr("height", $(window).height())
+    	.attr("width", bounds).attr("height", $(window).height()-10)
     	.attr("pointer-events", "all");
 
     var birth_range = $("#birthdates").val();
