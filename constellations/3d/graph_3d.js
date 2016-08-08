9a5db1753716e1cebff4f6f3768b2bf992e2ea2d
Louis Goddard <louisgoddard@gmail.com>
2016-08-08 14:21:18 +0100

    switched 3d graph library to jgraph

--- constellations/3d/graph_3d.js
+++ constellations/3d/graph_3d.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
 
     var nodes = JSON.parse(sessionStorage.getItem("nodes"));
     var links = JSON.parse(sessionStorage.getItem("links"));
-    var graph = {nodes: nodes, links: links};
-    var forceTHREE = new D3THREE();
-    forceTHREE.init("graph-3d");
-    var forceViz = new D3THREE.Force(forceTHREE);
-    forceTHREE.render(forceViz, graph);
-    forceTHREE.animate();
+    var graph = {nodes: nodes, edges: links};
+
+    jgraph.create("#graph-3d", {
+        directed: false,
+        defaultNodeColor: 0xffff99,
+        defaultEdgeColor: 0xbbbbbb
+    });
+
+    jgraph.draw(graph); 
 
 });
